ff1cb20fd1cef0759028783d22e78fc7f39e7b2c
Nofirt <trifon.j.trifonov@gmail.com>
2016-09-10 15:59:57 +0300

    Commiting the Demo2 folder

---
+++ Demo2/demo2/js/contactspage.js
@@ -0,0 +1,14 @@
+
+//Contacts Page
+$(document).ready(function () {
+
+    $("header nav").hide();
+    $("header nav").fadeIn(700);
+    $("#map-container").hide();
+    $("#map-container").slideDown(700);
+    $("#infotext").hide();
+    $("#infotext").show(700);
+    $("#contacts-info").hide();
+    $("#contacts-info").show(700);
+
+});
